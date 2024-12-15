91366d00f0b93c5bbc2abbb7afa846d2c82a0097
proj <lvhaost2004@gmail.com>
2024-12-15 15:48:14 +0700

    projtest

---
+++ precache-manifest.6a26335e96afdf9cf9f8a2c9f69d6c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8badecaf85dcbfa982e953cbd798c982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df61643485929226b18f",
+    "url": "/static/css/main.15bdd851.chunk.css"
+  },
+  {
+    "revision": "e14469b42b5b383e5113",
+    "url": "/static/js/2.23a3f531.chunk.js"
+  },
+  {
+    "revision": "00a0c8f113ef139e5b17d28db2455a34",
+    "url": "/static/js/2.23a3f531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df61643485929226b18f",
+    "url": "/static/js/main.d61f6cef.chunk.js"
+  },
+  {
+    "revision": "cb281c89ee4a04904c84",
+    "url": "/static/js/runtime-main.74f99036.js"
+  }
+]);
